509b1d7ecb46dd229425be3ac7fbc9f0221ce661
Alejan12 <alejandro.egidojimenez@usp.ceu.es>
2023-01-09 17:49:23 +0100

    subir nuevo

--- ejercicio7.py
+++ ejercicio7.py
@@ -7,5 +7,17 @@ def generar():
         batallon= random.choice(legiones)
         soldado= f"{batallon}-{numeros}"
         stroomtroper.append(soldado)
-    return stroomtroper
+        
+    soldado={}
+    for trooper in stroomtroper:
+        numeros=trooper[-3:]
+        if numeros not in soldado:
+            soldado[numeros]=[]
+        soldado[numeros].append(trooper)
+    soldado_legio={}
+    for trooper in stroomtroper:
+        legion=stroomtroper[:2]
+        if legion not in soldado_legio:
+            soldado_legio[legion]=[]
+        soldado_legio[legion].append(trooper)
 
